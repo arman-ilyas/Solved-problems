abc6c5c741dd33c9a0bdf6120a1f6272ebcb0fce
NyghmetElemesov <arman987@gmail.com>
2016-10-23 02:16:43 +0800

    added one more solution
    added solution for the staircase problem

---
+++ HackerRank/Algorithms/warmup/Staircase.java
@@ -0,0 +1,22 @@
+package com.practice.hackerrank;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= i; k++) {
+				System.out.print("#");
+			}
+			System.out.println("");
+		}
+		
+	}
+}
