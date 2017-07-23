4dcfbd622bf01adef1a02d6fb3de2338ba347919
tim <tim@tim.com>
2017-07-23 21:19:29 +0800

    solutions for hackerrank algorithm problems

---
+++ HackerRank/Algorithms/implementation/DrawingBook.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Tim Arman on 23/7/17.
+ */
+public class DrawingBook {
+}
---
+++ HackerRank/Algorithms/implementation/SaveThePrisoner.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Tim Arman on 9/7/17.
+ */
+public class SaveThePrisoner {
+}
